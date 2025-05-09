23fd36b6165864926a36dbb56191120b09c0aa3e
SolomiaK <solomia-chornenka@ukr.net>
2025-05-09 11:46:01 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,8 +20,7 @@ export default function RootLayout({ children }) {
 
         {/* ✅ Додаємо зовнішній скрипт */}
 <Script
-  async
-  src="https://stage-widget.intelswift.com/script.js?tenantId=3a30671b-cf91-4b10-8320-3ea4b5a8ad11&botId=6819d9712ec31b95a5dea40f&uuid=93cda7bf-2a0f-491a-872b-c3b89f0d1a9e&end=true"
+  async src="https://stage-widget.intelswift.com/script.js?tenantId=4c681175-e907-4369-9805-57b2a3212969&botId=681dc04bad835bfa88c632c6&uuid=9530ff16-b13e-4cf9-83b3-ad37b7005dc1&end=true"
   strategy="afterInteractive"
 />
 
